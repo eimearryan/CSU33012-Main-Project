7dfe6a32a934c7c517db3d6452592bd860adf465
eimearryan <ryane38@tcd.ie>
2022-11-18 10:36:46 +0000

    initial test class

---
+++ src/JSONDataTest.java
@@ -0,0 +1,16 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class JSONDataTest {
+
+    @org.junit.jupiter.api.Test
+    void pullHistory() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void commitHistory() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void contributors() {
+    }
+}
