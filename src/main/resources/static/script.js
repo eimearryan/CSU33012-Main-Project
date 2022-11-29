fd888e54b57d6379b5b838b6be47d6e1ed6c75c6
Hassan Alekri <85484546+alekrih@users.noreply.github.com>
2022-11-29 14:38:13 +0000

    Created more charts and tidied up UI

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,108 +1,29 @@
-// google.charts.load('current', {'packages':['corechart']});
-// google.charts.setOnLoadCallback(drawPieChart);
-// function drawPieChart() {
-//   var data = google.visualization.arrayToDataTable([
-//     ['Task', 'Hours per Day'],
-//     ['Work',     11],
-//     ['Eat',      2],
-//     ['Commute',  2],
-//     ['Watch TV', 2],
-//     ['Sleep',    7]
-//   ]);
-//   var options = {
-//     title: 'My Daily Activities',
-//     pieHole: 0.4,
-//     pieSliceText: "none",
-//     backgroundColor: "transparent",
-//     legend: {textStyle: {color: 'white'}},
-//     titleTextStyle: {color: 'white'},
-//     animation: {
-//       duration: 1000,
-//       easing: 'in',
-//       startup: true
-//     }
-//   };
-//   var chart = new google.visualization.PieChart(document.getElementById('piechart'));
-//   chart.draw(data, options);
-//   var percent = 0;
-//   var handler = setInterval(function(){
-//     // values increment
-//     percent += 1;
-//     // apply new values
-//     data.setValue(0, 1, percent);
-//     data.setValue(1, 1, 100 - percent);
-//     // update the pie
-//     chart.draw(data, options);
-//     // check if we have reached the desired value
-//     if (percent > 74) clearInterval(handler);
-//   }, 30);
-// }
-
-// google.charts.load('current', {'packages':['corechart']});
-// google.charts.setOnLoadCallback(drawColumnChart);
-// function drawColumnChart() {
-//   var data = google.visualization.arrayToDataTable([
-//     ["Element", "Density"],
-//     ["Copper", 8.94],
-//     ["Silver", 10.49],
-//     ["Gold", 19.30],
-//     ["Platinum", 21.45]
-//   ]);
-
-//   var options = {
-//     title: "Density of Precious Metals, in g/cm^3",
-//     width: 600,
-//     height: 400,
-//     bar: {groupWidth: "95%"},
-//     legend: { position: "none",
-//     textStyle: {color: 'white'}},
-//     backgroundColor: "transparent",
-//     chartArea:{backgroundColor: "transparent"},
-//     titleTextStyle: {color: 'white'},
-//     vAxis: {
-//       textStyle:{color: 'white'}
-//     },
-//     hAxis: {
-//       textStyle:{color: 'white'}
-//     },
-//     animation: {
-//       duration: 1000,
-//       easing: 'in',
-//       startup: true
-//     }
-//   };
-//   var chart = new google.visualization.ColumnChart(document.getElementById("columnchart_values"));
-//   chart.draw(data, options);
-// }
-
 google.charts.load('current', {'packages':['bar']});
-google.charts.setOnLoadCallback(newChart);
-function newChart(){
+google.charts.setOnLoadCallback(openedIssues);
+function openedIssues(){
   var jsonUnchanged = document.getElementById("jsonDataOpen").value;
-  
+
   console.log(jsonUnchanged);//
-  
+
   var issues = JSON.parse(jsonUnchanged);
-  
+
   console.log(issues);
-  
+
   var data = new google.visualization.DataTable();
   data.addColumn('string', 'User');
   data.addColumn('number', 'Opened Issues');
-  data.addColumn('number', 'Closed Issues');
-  
+
   console.log(data);
 
   for (var i = 0; i < issues.length; i++) {
-    data.addRow([issues[i].name, issues[i].open, issues[i].closed]);
+    if (issues[i].open > 0) data.addRow([issues[i].name, issues[i].open]);
   }
-  
+  data.sort({column: 1});
   console.log(data);
 
   var options = {
-    title: "Open vs Closed Issues per User",
-    width: 1300,
-    height: 900,
+    title: "Open Issues per User",
+    width: 700,
     bar: {groupWidth: "95%"},
     legend: { position: "none",
     textStyle: {color: 'white'}},
@@ -122,39 +43,92 @@ function newChart(){
     //   startup: true
     // }
   };
-  var chart = new google.charts.Bar(document.getElementById('chartData'));
+  var chart = new google.charts.Bar(document.getElementById('chartDataOpen'));
   chart.draw(data, google.charts.Bar.convertOptions(options));
 }
 
 google.charts.load('current', {'packages':['bar']});
-google.charts.setOnLoadCallback(addDelChart);
-function addDelChart(){
+google.charts.setOnLoadCallback(closedIssues);
+function closedIssues(){
+  var jsonUnchanged = document.getElementById("jsonDataOpen").value;
+
+  console.log(jsonUnchanged);//
+
+  var issues = JSON.parse(jsonUnchanged);
+
+  console.log(issues);
+
+  var data = new google.visualization.DataTable();
+  data.addColumn('string', 'User');
+  data.addColumn('number', 'Closed Issues');
+
+  console.log(data);
+
+  for (var i = 0; i < issues.length; i++) {
+    if ( issues[i].closed > 0 ) data.addRow([issues[i].name, issues[i].closed]);
+  }
+  data.sort({column: 1});
+  console.log(data);
+
+  var options = {
+    title: "Closed Issues per User",
+    width: 700,
+    bar: {groupWidth: "95%"},
+    legend: { position: "none",
+    textStyle: {color: 'white'}},
+    backgroundColor: "transparent",
+    chartArea:{backgroundColor: "transparent"},
+    titleTextStyle: {color: 'white'},
+    vAxis: {
+      textStyle:{color: 'white'}
+    },
+    hAxis: {
+      textStyle:{color: 'white'}
+    },
+    colors: ['#D3D3D3'],
+    // isStacked: true,
+    // animation: {
+    //   duration: 1000,
+    //   easing: 'in',
+    //   startup: true
+    // }
+  };
+  var chart = new google.charts.Bar(document.getElementById('chartDataClosed'));
+  chart.draw(data, google.charts.Bar.convertOptions(options));
+}
+
+google.charts.load('current', {'packages':['bar']});
+google.charts.setOnLoadCallback(addChart);
+function addChart(){
   //var jsonUnchanged = document.getElementById("jsonDataIssues").value;
-  
+
   //console.log(jsonUnchanged);//
-  
+
   //var issues = JSON.parse(jsonUnchanged);
-  
+
   //console.log(issues);
-  
+
   var jsonUnchanged = document.getElementById("jsonDataContrib").value;
   var contributions = JSON.parse(jsonUnchanged);
   console.log(contributions);
-  
+
   var data = new google.visualization.DataTable();
   data.addColumn('string', 'User');
   data.addColumn('number', 'Additions');
-  data.addColumn('number', 'Deletions');
 
   for (var i = 0; i < contributions.length; i++) {
-    data.addRow([contributions[i].user, contributions[i].additions, contributions[i].deletions]);
+    if ( contributions[i].additions > 2000 ) data.addRow([contributions[i].user, contributions[i].additions]);
   }
 
+  data.sort({column: 1});
+
   var options = {
-    title: "Commits",
-    width: 1300,
-    height: 900,
-    bar: {groupWidth: "95%"},
+    title: "Additions per User",
+    width: 700,
+    bar: {
+      groupWidth: "95%",
+    },
+    colors: ['#7CFC00'],
     legend: { position: "none",
     textStyle: {color: 'white'}},
     backgroundColor: "transparent",
@@ -173,7 +147,61 @@ function addDelChart(){
     //   startup: true
     // }
   };
-  var chart = new google.charts.Bar(document.getElementById('chartDataAddDel'));
+  var chart = new google.charts.Bar(document.getElementById('chartDataAdd'));
+  chart.draw(data, google.charts.Bar.convertOptions(options));
+}
+
+google.charts.load('current', {'packages':['bar']});
+google.charts.setOnLoadCallback(delChart);
+function delChart(){
+  //var jsonUnchanged = document.getElementById("jsonDataIssues").value;
+
+  //console.log(jsonUnchanged);//
+
+  //var issues = JSON.parse(jsonUnchanged);
+
+  //console.log(issues);
+
+  var jsonUnchanged = document.getElementById("jsonDataContrib").value;
+  var contributions = JSON.parse(jsonUnchanged);
+  console.log(contributions);
+
+  var data = new google.visualization.DataTable();
+  data.addColumn('string', 'User');
+  data.addColumn('number', 'Deletions');
+
+  for (var i = 0; i < contributions.length; i++) {
+    if ( contributions[i].deletions > 2000 ) data.addRow([contributions[i].user, contributions[i].deletions]);
+  }
+
+  data.sort({column: 1});
+
+  var options = {
+    title: "Deletions per User",
+    width: 700,
+    bar: {
+      groupWidth: "95%",
+    },
+    colors: ['red'],
+    legend: { position: "none",
+    textStyle: {color: 'white'}},
+    backgroundColor: "transparent",
+    chartArea:{backgroundColor: "transparent"},
+    titleTextStyle: {color: 'white'},
+    vAxis: {
+      textStyle:{color: 'white'}
+    },
+    hAxis: {
+      textStyle:{color: 'white'}
+    },
+    // isStacked: true,
+    // animation: {
+    //   duration: 1000,
+    //   easing: 'in',
+    //   startup: true
+    // }
+  };
+  var chart = new google.charts.Bar(document.getElementById('chartDataDel'));
   chart.draw(data, google.charts.Bar.convertOptions(options));
 }
 
@@ -183,22 +211,23 @@ function commitChart(){
   var jsonUnchanged = document.getElementById("jsonDataContrib").value;
   var contributions = JSON.parse(jsonUnchanged);
   console.log(contributions);
-  
+
   var data = new google.visualization.DataTable();
   data.addColumn('string', 'User');
   data.addColumn('number', 'Commits');
 
   for (var i = 0; i < contributions.length; i++) {
+    if ( contributions[i].total > 1 )
     data.addRow([contributions[i].user, contributions[i].total]);
   }
 
+  data.sort({column: 1});
+
   var options = {
     title: "Commits",
-    width: 1300,
-    height: 900,
+    width: 700,
     bar: {groupWidth: "95%"},
-    legend: { position: "none",
-    textStyle: {color: 'white'}},
+    legend: { position: "none"},
     backgroundColor: "transparent",
     chartArea:{backgroundColor: "transparent"},
     titleTextStyle: {color: 'white'},
@@ -219,8 +248,6 @@ function commitChart(){
   chart.draw(data, google.charts.Bar.convertOptions(options));
 }
 
-
-
 google.charts.load('current', {'packages':['corechart']});
 google.charts.setOnLoadCallback(percentageChart);
 function percentageChart() {
@@ -236,13 +263,14 @@ function percentageChart() {
   for (var i = 0; i < 5; i++) {
     if ( i < percentage.length ) data.addRow([percentage[i].name, percentage[i].closed]);
   }
-  
+
   var options = {
-    title: 'Top Users (Most Issues Resolved)',
+    title: 'Top Five Users (Most Issues Resolved)',
+    width:  700,
     pieHole: 0.4,
     pieSliceText: "none",
     backgroundColor: "transparent",
-    legend: {textStyle: {color: 'white'}},
+    legend: { position: "none"},
     titleTextStyle: {color: 'white'},
     animation: {
       duration: 1000,
